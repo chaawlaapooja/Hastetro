3197a72f986668c1b6eb6d4274eb5d7a639fa8a8
Pooja Chawla <pooja@Poojas-MacBook-Air.local>
2019-08-05 16:25:09 +0530

    changing image sizes

--- imports/ui/LandingPage.jsx
+++ imports/ui/LandingPage.jsx
@@ -3,30 +3,30 @@ import React from 'react';
 class LandingPage extends React.Component{
 
 	componentDidMount(){
-// 		var message="Right-click has been disabled";
-// function clickIE() {
-//     if (document.all) {
-//         (message);
-//         return false;
-//     }
-// }
-// function clickNS(e) {
-//     if (document.layers || (document.getElementById && !document.all)) {
-//         if (e.which == 2||e.which == 3) {
-//             (message);
-//             return false;
-//         }
-//     }
-// }
-// if (document.layers) {
-//     document.captureEvents(Event.MOUSEDOWN);
-//     document.onmousedown = clickNS;
-// } else {
-//     document.onmouseup = clickNS;
-//     document.oncontextmenu = clickIE;
-// }
-// document.oncontextmenu = new Function("return false");
-// document.getElementsByClassName('my-img').ondragstart = function() { return false; };
+		var message="Right-click has been disabled";
+function clickIE() {
+    if (document.all) {
+        (message);
+        return false;
+    }
+}
+function clickNS(e) {
+    if (document.layers || (document.getElementById && !document.all)) {
+        if (e.which == 2||e.which == 3) {
+            (message);
+            return false;
+        }
+    }
+}
+if (document.layers) {
+    document.captureEvents(Event.MOUSEDOWN);
+    document.onmousedown = clickNS;
+} else {
+    document.onmouseup = clickNS;
+    document.oncontextmenu = clickIE;
+}
+document.oncontextmenu = new Function("return false");
+document.getElementsByClassName('my-img').ondragstart = function() { return false; };
 
 		
 	}
@@ -67,32 +67,32 @@ class LandingPage extends React.Component{
 </li><br/>
       <li>We are not only providing the products for direct selling but we ourselves use these products in our day-to-day life. Come, have a look.</li><br/>
       </h4>
-      <h3 className="heading">ABOUT BOARD OF DIRECTORS</h3>
+      <h3 className="heading">BOARD OF DIRECTORS</h3>
       <div className="content">
       	<div className="cardContainer">
       	<div className="card bg-primary" style={{width:60+"%", padding:6+'%'}}>
-		  <img className="card-img-top" src="https://res.cloudinary.com/luckymobile/image/upload/v1564932249/hastetro/Ashokrao_Khot.jpg" className="img-thumbnail" alt="Ashokrao_Khot"/>
+		  <img className="card-img-top" src="https://res.cloudinary.com/luckymobile/image/upload/v1564932249/hastetro/Ashokrao_Khot.jpg" className="img-thumbnail" alt="Ashokrao_Khot" height="200" width="200"/>
 		  <div className="card-body">
 		    <h4 className="card-title" style={{color:'red', fontSize:20+'px'}}>Ashokrao Khot, CMD</h4>
-		    <p className="card-text">A dreamy Mr. Ashokrao Khot realized that working for any other organization could be a barrier to achieving big dreams. So he decided to start his own business. Mr. Khot is always looking for perfection in everything he or his company does. For more than 10 years, the leader of the direct sells industry, Shri. Ashokrao Khot helped thousands of people to realize their dreams.</p>
+		    <p className="card-text">The man of dreams, Mr. Ashokrao Khot realized that working for any other organization could be a barrier to achieving big dreams. So he decided to start his own business. Mr. Khot is always looking for perfection in everything he or his company does. For more than 10 years, the leader of the direct sells industry, Shri. Ashokrao Khot helped thousands of people to realize their dreams.</p>
     	  </div>
     	</div>
     	<div className="card bg-primary" style={{width:60+"%", padding:6+'%'}}>
-		  <img className="card-img-top" src="https://res.cloudinary.com/luckymobile/image/upload/v1564932320/hastetro/Santosh_Jadhav.jpg" className="img-thumbnail" alt="Santosh_Jadhav"/>
+		  <img className="card-img-top" src="https://res.cloudinary.com/luckymobile/image/upload/v1564932320/hastetro/Santosh_Jadhav.jpg" className="img-thumbnail" alt="Santosh_Jadhav" height="200" width="200"/>
 		  <div className="card-body">
 		    <h4 className="card-title" style={{color:'red', fontSize:20+'px'}}>Santosh Jadhav, Vice President</h4>
 		    <p className="card-text">A Visionary Man with the outstanding experience in marketing company.</p>
     	  </div>
     	</div>
     	<div className="card bg-primary" style={{width:60+"%", padding:6+'%'}}>
-		  <img className="card-img-top" src="https://res.cloudinary.com/luckymobile/image/upload/v1564938350/hastetro/2.jpg" className="img-thumbnail" alt="Akshay_Mugalkhod"/>
+		  <img className="card-img-top" src="https://res.cloudinary.com/luckymobile/image/upload/v1564938350/hastetro/2.jpg" className="img-thumbnail" alt="Akshay_Mugalkhod" height="200" width="200"/>
 		  <div className="card-body">
 		    <h4 className="card-title" style={{color:'red', fontSize:20+'px'}}>Akshay Mugalkhod, Director</h4>
 		    <p className="card-text">A man with the dream of empowering the people of India with the vision of making everyone financially independent. </p>
     	  </div>
     	</div>
     	<div className="card bg-primary" style={{width:60+"%", padding:6+'%'}}>
-		  <img className="card-img-top" src="https://res.cloudinary.com/luckymobile/image/upload/v1564932289/hastetro/Suraj_Sagare.jpg" className="img-thumbnail" alt="Suraj_Sagare"/>
+		  <img className="card-img-top" src="https://res.cloudinary.com/luckymobile/image/upload/v1564932289/hastetro/Suraj_Sagare.jpg" className="img-thumbnail" alt="Suraj_Sagare" height="200" width="200"/>
 		  <div className="card-body">
 		    <h4 className="card-title" style={{color:'red', fontSize:20+'px'}}>Suraj Sagare, Development Director (Technical)</h4>
 		    <p className="card-text">A person with the amazing experience in technical Development and product enhancement has always made his efforts upto the mark to raise the company in all fields.</p>
@@ -143,6 +143,8 @@ class LandingPage extends React.Component{
     </div>
     <div id="menuProducts" className="tab-pane fade">
       <h3 className="heading">OUR PRODUCTS</h3>
+      	<div style={{paddingLeft:2+"%", paddingRight:2+'%', marginLeft:10+'%', marginRight:10+'%', display: 'flex',
+    justifyContent: 'center'}}>
     	<div id="myCarousel" className="carousel slide" data-ride="carousel">
 	    <ol className="carousel-indicators">
 	      <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
@@ -155,7 +157,7 @@ class LandingPage extends React.Component{
 	    <div className="carousel-inner">
 
 	      <div className="item active">
-	        <img src="https://res.cloudinary.com/luckymobile/image/upload/c_scale,h_700,w_1200/v1564856267/hastetro/Organic_Powar.jpg" alt="organic Power" style={{width:100+'%'}}/>
+	        <img src="https://res.cloudinary.com/luckymobile/image/upload/c_scale,h_700,w_500/v1564856267/hastetro/Organic_Powar.jpg" alt="organic Power" style={{width:100+'%'}}/>
 	        <div className="carousel-caption">
 	          <h3 style={{color:'black'}}>Orgainic Power</h3>
 	          <p style={{color:'black'}}>Organic powder for agricultural use!</p>
@@ -163,7 +165,7 @@ class LandingPage extends React.Component{
 	      </div>
 
 	      <div className="item">
-	        <img src="https://res.cloudinary.com/luckymobile/image/upload/c_scale,h_700,w_1200/v1564856236/hastetro/canegld.jpg" alt="Canegold" style={{width:100+'%'}}/>
+	        <img src="https://res.cloudinary.com/luckymobile/image/upload/c_scale,h_700,w_500/v1564856236/hastetro/canegld.jpg" alt="Canegold" style={{width:100+'%'}}/>
 	        <div className="carousel-caption">
 	          <h3 style={{color:'black'}}>Canegold</h3>
 	          <p style={{color:'black'}}>For agricultural use!</p>
@@ -171,7 +173,7 @@ class LandingPage extends React.Component{
 	      </div>
 	    
 	      <div className="item">
-	        <img src="https://res.cloudinary.com/luckymobile/image/upload/c_scale,h_700,w_1200/v1564856288/hastetro/Spirulina.jpg" alt="Spirulina" style={{width:100+'%'}}/>
+	        <img src="https://res.cloudinary.com/luckymobile/image/upload/c_scale,h_700,w_500/v1564856288/hastetro/Spirulina.jpg" alt="Spirulina" style={{width:100+'%'}}/>
 	        <div className="carousel-caption">
 	          <h3 style={{color:'black'}}>Spirulina</h3>
 	          <p style={{color:'black'}}>Muscle strengthening tablets!</p>
@@ -179,7 +181,7 @@ class LandingPage extends React.Component{
 	      </div>
 
 	      <div className="item">
-	        <img src="https://res.cloudinary.com/luckymobile/image/upload/c_scale,h_700,w_1200/v1564856248/hastetro/LEDY_SAFE1.jpg" alt="Lady Safe" style={{width:100+'%'}}/>
+	        <img src="https://res.cloudinary.com/luckymobile/image/upload/c_scale,h_700,w_500/v1564856248/hastetro/LEDY_SAFE1.jpg" alt="Lady Safe" style={{width:100+'%'}}/>
 	        <div className="carousel-caption">
 	          <h3 style={{color:'black'}}>Lady Safe</h3>
 	          <p style={{color:'black'}}>Sanitary Napkins!</p>
@@ -187,7 +189,7 @@ class LandingPage extends React.Component{
 	      </div>
 
 	      <div className="item">
-	        <img src="https://res.cloudinary.com/luckymobile/image/upload/c_scale,h_700,w_1200/v1564856254/hastetro/Organic_Powar_Gold.jpg" alt="Organic Power Gold" style={{width:100+'%'}}/>
+	        <img src="https://res.cloudinary.com/luckymobile/image/upload/c_scale,h_700,w_500/v1564856254/hastetro/Organic_Powar_Gold.jpg" alt="Organic Power Gold" style={{width:100+'%'}}/>
 	        <div className="carousel-caption">
 	          <h3 style={{color:'black'}}>Organic Power gold</h3>
 	          <p style={{color:'black'}}>Powder for agricultural use!</p>
@@ -205,6 +207,7 @@ class LandingPage extends React.Component{
 	      <span className="sr-only">Next</span>
 	    </a>
 	</div>
+	</div>
     </div>
     <div id="menuRules" className="tab-pane fade">
       <h3 className="heading">RULES AND POLICIES</h3>
@@ -221,6 +224,7 @@ class LandingPage extends React.Component{
       Hastetro Trade Private Limited is goverment issued MSME.<br/>
       We ensure consumer safety and unlike other companies, we have all legal documents from income tax department to keep your returns and profits safe.<br/>
       Have a look at all our legal documents and join us to have your safe and secure financial future.</h4>
+      	<div style={{display:'flex', justifyContent:'center'}}>
       	<div id="myCarousel1" className="carousel slide" data-ride="carousel">
 	    <ol className="carousel-indicators">
 	      <li data-target="#myCarousel1" data-slide-to="0" className="active"></li>
@@ -231,21 +235,21 @@ class LandingPage extends React.Component{
 	    <div className="carousel-inner">
 
 	      <div className="item active">
-	        <img src="https://res.cloudinary.com/hastetro/image/upload/c_fill,g_auto,h_700,w_1200/b_rgb:000000,e_gradient_fade,y_-0.50/c_scale,co_rgb:d9d9d9,fl_relative,l_text:montserrat_25_style_light_align_center:HASTETRO,w_0.5,y_0.18/v1564838344/PAN_drmwwk.png" alt="PAN" style={{width:100+'%'}}/>
+	        <img src="https://res.cloudinary.com/hastetro/image/upload/c_fill,g_auto,h_500,w_800/b_rgb:000000,e_gradient_fade,y_-0.50/c_scale,co_rgb:d9d9d9,fl_relative,l_text:montserrat_25_style_light_align_center:HASTETRO,w_0.5,y_0.18/v1564838344/PAN_drmwwk.png" alt="PAN" style={{width:100+'%'}}/>
 	        <div className="carousel-caption">
 	          <h3>PAN CARD</h3>
 	        </div>
 	      </div>
 
 	      <div className="item" >
-	        <img className='msme' src="https://res.cloudinary.com/hastetro/image/upload/c_fill,g_auto,h_700,w_1200/b_rgb:000000,e_gradient_fade,y_-0.50/c_scale,co_rgb:d9d9d9,fl_relative,l_text:montserrat_25_style_light_align_center:HASTETRO,w_0.5,y_0.18/v1564838325/MSME_z8tbst.png" alt="MSME CERTIFICATE" style={{width:100+'%'}}/>
+	        <img className='msme' src="https://res.cloudinary.com/hastetro/image/upload/c_fill,g_auto,h_500,w_800/b_rgb:000000,e_gradient_fade,y_-0.50/c_scale,co_rgb:d9d9d9,fl_relative,l_text:montserrat_25_style_light_align_center:HASTETRO,w_0.5,y_0.18/v1564838325/MSME_z8tbst.png" alt="MSME CERTIFICATE" style={{width:100+'%'}}/>
 	        <div className="carousel-caption">
 	          <h3>MSME CERTIFICATE</h3>
 	        </div>
 	      </div>
 
 	      <div className="item">
-	        <img src="https://res.cloudinary.com/hastetro/image/upload/c_fill,g_auto,h_700,w_1200/b_rgb:000000,e_gradient_fade,y_-0.50/c_scale,co_rgb:d9d9d9,fl_relative,l_text:montserrat_25_style_light_align_center:HASTETRO,w_0.5,y_0.18/v1564838313/Certificate_of_Inc_aokcwa.png" alt="CERTIFICATE OF INCORPORATION" style={{width:100+'%'}}/>
+	        <img src="https://res.cloudinary.com/hastetro/image/upload/c_fill,g_auto,h_500,w_800/b_rgb:000000,e_gradient_fade,y_-0.50/c_scale,co_rgb:d9d9d9,fl_relative,l_text:montserrat_25_style_light_align_center:HASTETRO,w_0.5,y_0.18/v1564838313/Certificate_of_Inc_aokcwa.png" alt="CERTIFICATE OF INCORPORATION" style={{width:100+'%'}}/>
 	        <div className="carousel-caption">
 	          <h3>CERTIFICATE OF INCORPORATION</h3>
 	        </div>
@@ -261,7 +265,7 @@ class LandingPage extends React.Component{
 	      <span style={{color:'black'}} className="glyphicon glyphicon-chevron-right"></span>
 	      <span className="sr-only">Next</span>
 	    </a>
-	  
+	</div>
 	</div>
     </div>
     <div id="menuContact" className="tab-pane fade">
